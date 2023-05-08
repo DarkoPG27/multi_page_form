17aa1ea26618bc5e8e32a7a596b3ad8a77b1050e
DarkoPG27 <darkobrnovic27@gmail.com>
2023-05-08 11:31:16 +0200

    Added  FormContext

---
+++ src/context/FormContext.js
@@ -0,0 +1,13 @@
+import { createContext, useState, useEffet } from "react";
+
+const FormContext = createContext({});
+
+export const FormProvider = ({ children }) => {
+    return (
+        <FormContext.Provider value={{}} >
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+export default FormContext;
